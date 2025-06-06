f2f8719a7b094ee69195e57b6234e54efc6daf8b
Neal Maker <nealmaker@gmail.com>
2025-06-06 12:14:26 -0400

    more analysis of preliminary marteloscope results

--- R/explore-output.R
+++ R/explore-output.R
@@ -65,25 +65,68 @@ View(sim$trees[map_inx,] |> filter(entry, year != regen_yr) |>
 # remove red maples over sugar maples, unless they're a lot bigger
 
 
+# Patterns to stocking? ########################################################
+sim$trees |> group_by(plot, year) |> 
+  summarize(ba = sum(ba_ac * cumsurv), tpa = sum(tpa * cumsurv)) |> 
+  full_join(data.frame(plot = sim$plots$plot)) |> 
+  mutate(ba = if_else(is.na(ba), 0, ba),
+         tpa = if_else(is.na(tpa), 0, tpa)) |> 
+  # group_by(year) |> 
+  # summarize(ba = mean(ba)) |> 
+  ggplot(aes(year, ba)) + geom_line() + facet_wrap(~plot)
+# at each entry, thin any neigborhood that got over 200 sq ft / ac
+# & remove ~ 25-35 % of the stocking
 
 
-sim$trees |> dplyr::filter(year %% (2 * sim$params$steplength) == 0) |> 
-  dplyr::group_by(year, plot) |> 
-  dplyr::summarize(ba = sum(ba_ac * cumsurv)) |> 
-  ggplot2::ggplot(ggplot2::aes(ba)) +
-  ggplot2::geom_histogram() +
-  ggplot2::facet_wrap(~ year)
-  
-opt$trees |> dplyr::group_by(plot) |> 
-  dplyr::summarize(regen_yr = max(cutyr)) |> 
-  dplyr::group_by(regen_yr) |> 
-  dplyr::summarise(pct = 100 * dplyr::n() / nrow(opt$plots))
+# Patterns to regeneration? ####################################################
+ptemp |> group_by(regen_yr) |> 
+  summarize(pct_regenerated = n() / nrow(ptemp))
+# regenerating 15% of stand right away
+# then another 10% after about 160 years
+# the remaining 75% regenerated between 180 and 210 years
 
-View(sim$trees |> 
-       dplyr::group_by(plot, year) |> 
-       dplyr::summarize(taps = sum(taps * tpa * cumsurv), 
-                        ba = sum(ba_ac * cumsurv)))
+View(sim$trees |> filter(year == 0) |> group_by(plot) |> 
+  summarize(tpa_hm = sum(tpa[spp == "hard maple"]),
+            tpa_rm = sum(tpa[spp == "red maple"]),
+            tpa_map = sum(tpa[spp %in% c("hard maple", "red maple")]),
+            regen_year = regen_yr[1]) |> 
+  arrange(regen_year))
+# regenerate any neighborhood w/ insuficient maple production
+# (< 40 maple stems / ac when they're poles or small sawtimber; 
+# fewer stems is okay if they're bigger)
+# cut red maple crop trees around 46-48" dbh
+# cut hard maple crop trees around 41-44" dbh (but longer growing than red)
 
+View(sim$trees |> filter(year == regen_yr) |> group_by(plot) |> 
+       summarize(tpa_hm = sum(tpa[spp == "hard maple"] * cumsurv[spp == "hard maple"]),
+                 tpa_rm = sum(tpa[spp == "red maple"] * cumsurv[spp == "red maple"]),
+                 tpa_map = sum(tpa[spp %in% c("hard maple", "red maple")] * 
+                                 cumsurv[spp %in% c("hard maple", "red maple")]),
+                 regen_year = regen_yr[1]) |> 
+       arrange(regen_year))
+
+sim$trees |> filter(year == regen_yr, regen_yr > 100) |> group_by(plot) |> 
+  summarize(tpa_hm = sum(tpa[spp == "hard maple"] * cumsurv[spp == "hard maple"]),
+            tpa_rm = sum(tpa[spp == "red maple"] * cumsurv[spp == "red maple"]),
+            tpa_map = sum(tpa[spp %in% c("hard maple", "red maple")] * 
+                            cumsurv[spp %in% c("hard maple", "red maple")]),
+            regen_year = regen_yr[1]) |> 
+  arrange(regen_year) |> 
+  ggplot(aes(regen_year, tpa_map)) + geom_point()
+
+sim$trees |> group_by(plot, year) |> 
+  summarize(ba = sum(ba_ac * cumsurv), tpa = sum(tpa * cumsurv), regen_yr = regen_yr[1]) |> 
+  full_join(data.frame(plot = sim$plots$plot)) |> 
+  mutate(ba = if_else(is.na(ba), 0, ba),
+         tpa = if_else(is.na(tpa), 0, tpa)) |> 
+  # group_by(year) |> 
+  # summarize(ba = mean(ba)) |> 
+  ggplot(aes(year, tpa)) + geom_line() + 
+  facet_wrap(~plot) +
+  scale_y_continuous(limits = c(0, 50))
+
+
+# evolution of tap density #####################################################
 sim$trees |> 
   dplyr::group_by(plot, year) |> 
   dplyr::summarize(taps = sum(taps * tpa * cumsurv), 
@@ -92,3 +135,25 @@ sim$trees |>
   dplyr::summarize(taps = mean(taps)) |> 
   ggplot2::ggplot(ggplot2::aes(year, taps)) +
   ggplot2::geom_line()
+
+
+# what does first cut look like ################################################
+firstcut <- sim$trees |> filter(year == 0) |> mutate(cut0 = cutyr == 0) |> 
+  group_by(plot) |> 
+  summarize(ba_kept = sum(ba_ac[!cut0] * cumsurv[!cut0]),
+            ba_cut = sum(ba_ac[cut0] * cumsurv[cut0]),
+            tpa_kept = sum(tpa[!cut0] * cumsurv[!cut0]),
+            tpa_cut = sum(tpa[cut0] * cumsurv[cut0])) |> 
+  mutate(pct_cut = 100 * ba_cut / (ba_cut + ba_kept))
+summary(firstcut)
+# 40% ba removed overall
+# 15% of area regenerated; 61% thinned, 24% left alone
+
+spp_first <- sim$trees |> filter(year == 0) |> mutate(cut0 = cutyr == 0) |> 
+  group_by(spp) |> 
+  summarise(ba = sum(ba_ac) / 34,
+            ba_cut = sum(ba_ac[cut0]) / 34) |>
+  mutate(pct_ba_cut = 100 * ba_cut / ba) |> 
+  arrange(desc(ba))
+spp_first$pct_total <- 100 * spp_first$ba / sum(spp_first$ba)
+# basically everything driven by removing non-maples
